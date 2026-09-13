b5f75ce20ef431664438f0db815752fc77803eaf
Ricardox <barandiavillca@alumno.huergo.edu.ar>
2026-09-13 20:45:33 -0300

    suma de todo

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 import pygame 
 import time
 import random
+=======
 import pygame
+>>>>>>> a13210ad661c49f7afc419087d3cb03c1d357157
 def menu():
     ejecutando = True
     pygame.init()
@@ -10,7 +12,6 @@ def menu():
     jugar = pygame.draw.rect(ventana, (200, 0, 0), (250, 120, 200, 80))
     ajustes = pygame.draw.rect(ventana, (0,200, 0), (250, 240, 200, 80))
     while ejecutando:
-
         ventana = pygame.display.set_mode((1000, 600))
         for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
@@ -49,6 +50,7 @@ def juego(ventana, ejecutando):
         direccion = ()        
 =======
         v = 5
+        velocidad = float(v)
         direccion = ()
 >>>>>>> a13210ad661c49f7afc419087d3cb03c1d357157
         for evento in pygame.event.get():
@@ -57,22 +59,19 @@ def juego(ventana, ejecutando):
         teclas = pygame.key.get_pressed()
         if teclas[pygame.K_z]:
              ejecutando = False
-        if teclas[pygame.K_w] and  personaje.top > 0:
         if teclas[pygame.K_w] and  personaje.top > 50:
             y -= velocidad
             direccion = "arriba"
-        if teclas[pygame.K_s] and  personaje.bottom < 600:
         if teclas[pygame.K_s] and  personaje.bottom < 550:
             y += velocidad
             direccion = "abajo"
-        if teclas[pygame.K_a] and  personaje.left > 0:
         if teclas[pygame.K_a] and  personaje.left > 50:
             x -= velocidad
             direccion = "izquierda"
-        if teclas[pygame.K_d] and personaje.right < 1000:
         if teclas[pygame.K_d] and personaje.right < 950:
             x += velocidad
             direccion = "derecha"
+<<<<<<< HEAD
         if xe < x:
             xe += velocidad_e
         elif xe > x:
@@ -90,7 +89,13 @@ def juego(ventana, ejecutando):
             ejecutando = False
         pygame.display.flip()
 
+=======
         ventana.fill((0, 0, 0))
         escenario = pygame.draw.rect(ventana, (255, 255, 255), (50, 50, 900,500))
+        personaje = pygame.draw.rect(ventana, (255, 60, 255), (x, y, 40, 40))
+        pygame.display.flip()
+        #reloj = pygame.time.clock()
+        #reloj.tick(60)
+>>>>>>> a13210ad661c49f7afc419087d3cb03c1d357157
 menu()
 pygame.quit()
