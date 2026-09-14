1bfafc51e3623ee67ee8a7ad7425d0fd9425ca94
Matt <mateonahuelrodriguez@gmail.com>
2026-09-13 21:00:42 -0300

    arregle el codigo, anadi escenario y fusione todo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import pygame 
 import time
 import random
@@ -7,11 +7,11 @@ def menu():
     ejecutando = True
     pygame.init()
     pygame.display.set_caption("pokemanic")
+    ventana = pygame.display.set_mode((1000, 600))
+    salir = pygame.draw.rect(ventana, (0,0, 200), (250, 360, 200, 80))
     jugar = pygame.draw.rect(ventana, (200, 0, 0), (250, 120, 200, 80))
     ajustes = pygame.draw.rect(ventana, (0,200, 0), (250, 240, 200, 80))
     while ejecutando:
-
-        ventana = pygame.display.set_mode((1000, 600))
         for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     ejecutando = False
@@ -31,13 +31,14 @@ def juego(ventana, ejecutando):
     x = 500
     y = 400
     cont = 0
-    xe = random.randint(0,1000)
-    ye = random.randint(0,600)
+    xe = random.randint(50, 950)
+    ye = random.randint(50,550)
     ultimo_golpe = 0
     tiempo_entre_golpes = 500
     while ejecutando:
-<<<<<<< HEAD
+
         ventana.fill((0, 0, 0))
+        escenario = pygame.draw.rect(ventana, (255, 255, 255, ), (50, 50, 900, 500 ))
         personaje = pygame.draw.rect(ventana, (255, 50, 255), (x, y, 75, 75))
         enemigo = pygame.draw.rect(ventana,(255, 0, 0),(xe , ye, 75 ,75 ) )
         pygame.draw.rect(ventana, (0, 200, 0), (20, 20, vida * 2, 25))
@@ -47,29 +48,24 @@ def juego(ventana, ejecutando):
         velocidad = 5
         velocidad_e = 2
         direccion = ()        
-=======
         v = 5
         direccion = ()
->>>>>>> a13210ad661c49f7afc419087d3cb03c1d357157
+
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 ejecutando = False
         teclas = pygame.key.get_pressed()
         if teclas[pygame.K_z]:
              ejecutando = False
-        if teclas[pygame.K_w] and  personaje.top > 0:
         if teclas[pygame.K_w] and  personaje.top > 50:
             y -= velocidad
             direccion = "arriba"
-        if teclas[pygame.K_s] and  personaje.bottom < 600:
         if teclas[pygame.K_s] and  personaje.bottom < 550:
             y += velocidad
             direccion = "abajo"
-        if teclas[pygame.K_a] and  personaje.left > 0:
         if teclas[pygame.K_a] and  personaje.left > 50:
             x -= velocidad
             direccion = "izquierda"
-        if teclas[pygame.K_d] and personaje.right < 1000:
         if teclas[pygame.K_d] and personaje.right < 950:
             x += velocidad
             direccion = "derecha"
@@ -91,6 +87,5 @@ def juego(ventana, ejecutando):
         pygame.display.flip()
 
         ventana.fill((0, 0, 0))
-        escenario = pygame.draw.rect(ventana, (255, 255, 255), (50, 50, 900,500))
 menu()
 pygame.quit()
